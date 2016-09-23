fdb685beac102737ad8b398df57b8906a129532d
superandrew123 <andrewmiksch@gmail.com>
2016-09-23 14:14:00 -0400

    some sort of clicking through the board is happening

--- app/assets/javascripts/components/gameBoard.js.jsx
+++ app/assets/javascripts/components/gameBoard.js.jsx
@@ -1,13 +1,23 @@
 var GameBoard = React.createClass({
   getInitialState() {
-      return {
-      };
+    return {
+    };
   },
+  
   render: function(){
     // 5x5 board to start
-    var tiles = 25;
+    var colors = [
+      "red", "blue", "green", "yellow"
+    ];
+    var tiles = [];
+    for(var i = 0; i < 25; i++){
+      tiles.push(colors[parseInt(Math.random() * colors.length)]);
+    }
     return(<div>
         {
+          tiles.map(function(color, e, colors){
+            return <Tile color={color} key={e} tileNumber={e} colors={colors}/>;
+          })
         }
         </div>
       )
--- app/assets/javascripts/components/tile.js.jsx
+++ app/assets/javascripts/components/tile.js.jsx
@@ -1,7 +1,23 @@
 
 var Tile = React.createClass({
-  
+  handleClick: function(event){
+    var tileNumber = this.props.tileNumber;
+    if(tileNumber == (this.props.colors.length - 1)){
+      var nextColor = this.props.colors[0];
+    } else {
+      var nextColor = this.props.colors[tileNumber + 1]
+    }
+    event.target.style.backgroundColor = nextColor;
+  },
   render: function(){
-    return <p>test</p>
+    var tileStyle = {
+      'backgroundColor': this.props.color
+    }
+    return (<div 
+          id={'tile' + this.props.tileNumber}
+          className="tile" 
+          style={tileStyle} 
+          onClick={this.handleClick}
+      />);
   }
 });
