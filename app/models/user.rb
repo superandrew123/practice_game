15e487109bc40d60e02e1c2622ca57ff20b6690e
superandrew123 <andrewmiksch@gmail.com>
2016-11-14 13:00:07 -0500

    unit testing for some statistics on users and more consistent url generation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ActiveRecord::Base
   private 
     def generate_url
       date_time = DateTime.new.to_s
-      self.url = Base64.urlsafe_encode64(rand(10000).to_s + 'x' + self.id.to_s)
+      self.url = Base64.urlsafe_encode64(rand(1000..9999).to_s + 'x' + self.id.to_s)
       self.save
     end
 end
